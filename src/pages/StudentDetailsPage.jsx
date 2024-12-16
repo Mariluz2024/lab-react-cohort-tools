f8cd5c2f34e4a17d0e0b875a26d230684ed2afda
Mariluz2024 <mluzco29@gmail.com>
2024-12-16 01:44:04 +0100

    done

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,31 +1,31 @@
+import { useParams, Link } from "react-router-dom";
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
 
 function StudentDetailsPage() {
+  // Obtener el parámetro dinámico `studentId` desde la URL
+  const { studentId } = useParams();
 
-  // Find the current student profile by id.
-  // In this case, the student with id 1. The `studentId` is hard-coded for now.
-  // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
+  // Buscar el perfil del estudiante en función del `studentId`
   const studentProfile = studentsData.find((student) => student._id === studentId);
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
       <h1>Student Details Page</h1>
       <div className="bg-white p-8 rounded-lg shadow-md mb-6">
-        {studentProfile && (
+        {studentProfile ? (
           <>
             <img
               src={studentProfile.image || placeholderImage}
               alt="profile-photo"
               className="rounded-full w-32 h-32 object-cover border-2 border-gray-300"
             />
-            <h1 className="text-2xl mt-4 font-bold absolute">
+            <h1 className="text-2xl mt-4 font-bold">
               {studentProfile.firstName} {studentProfile.lastName}
             </h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-24 mb-4 border-b pb-4">
-              <p className="text-left mb-2 border-b pb-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4 mb-4 border-b pb-4">
+              <p>
                 <strong>LinkedIn:</strong>{" "}
                 <a
                   href={studentProfile.linkedinUrl}
@@ -36,42 +36,39 @@ function StudentDetailsPage() {
                   {studentProfile.linkedinUrl}
                 </a>
               </p>
-
-              <p className="text-left mb-2 border-b pb-2">
+              <p>
                 <strong>Email:</strong>{" "}
                 <span className="ml-2 text-blue-500 hover:underline">
                   {studentProfile.email}
                 </span>
               </p>
-
-              <p className="text-left mb-2 border-b pb-2">
-                <strong>Languages:</strong>{" "}
-                {studentProfile.languages.join(", ")}
+              <p>
+                <strong>Languages:</strong> {studentProfile.languages.join(", ")}
               </p>
-
-              <p className="text-left mb-2 border-b pb-2">
+              <p>
                 <strong>Program:</strong> {studentProfile.program}
               </p>
-
-              <p className="text-left mb-2 pb-2">
+              <p>
                 <strong>Background:</strong> {studentProfile.background}
               </p>
-
-              <p className="text-left mb-2 pb-2">
-                <strong>Cohort:</strong>
+              <p>
+                <strong>Cohort:</strong>{" "}
                 <span className="ml-2 text-blue-500 hover:underline">
                   {studentProfile.cohort}
                 </span>
               </p>
             </div>
 
-
-            {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            {/* Botón para regresar a la página principal */}
+            <Link
+              to="/"
+              className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out"
+            >
               Back
-            </button>
-
+            </Link>
           </>
+        ) : (
+          <p className="text-red-500">Student not found.</p>
         )}
       </div>
     </div>
